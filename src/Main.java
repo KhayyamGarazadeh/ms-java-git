acd0881dd14091c05b7e645e246937f328ae59c1
KhayyamGarazadeh <xeyyamqarazade123@gmail.com>
2023-06-19 13:30:04 +0400

    BSA-23:Added hello ms15

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello ingress");
+        System.out.println("Hello ms14");
     }
 }
